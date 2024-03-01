d59c40eb0913fde05e2bfd6e344a2346e2cc4a14
Craig Holyoak <craig@helmsdeep.org>
2024-03-02 09:36:20 +1000

    Serveral updates, including a Docker upgrade and better IPv6 support

--- roles/docker_nginx/filter_plugins/filters.py
+++ roles/docker_nginx/filter_plugins/filters.py
@@ -1,5 +1,5 @@
 class FilterModule:  # pylint: disable=too-few-public-methods
     def filters(self):  # pylint: disable=no-self-use
         return {
-            "process_frontend_nets": lambda x: {y: {"external": {"name": y}} for y in x}
+            "process_frontend_nets": lambda x: {y: {"external": True, "name": y} for y in x}
         }
